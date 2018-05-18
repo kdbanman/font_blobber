5f8223083c9ea669060e3c02d51688db056fb0f7
Kirby Banman <kirby.banman@gmail.com>
2018-05-18 11:23:06 -0600

    improve readability slightly

--- src/components/textCanvas.js
+++ src/components/textCanvas.js
@@ -1,6 +1,6 @@
 
 
-var PIXEL_RATIO = (function () {
+var MAX_DEFINITION_PIXEL_RATIO = (function () {
     var ctx = document.createElement("canvas").getContext("2d"),
         dpr = window.devicePixelRatio || 1,
         bsr = ctx.webkitBackingStorePixelRatio ||
@@ -8,13 +8,13 @@ var PIXEL_RATIO = (function () {
               ctx.msBackingStorePixelRatio ||
               ctx.oBackingStorePixelRatio ||
               ctx.backingStorePixelRatio || 1;
-  
+
     return dpr / bsr;
   })();
-  
-  function setHiDPICanvas(canvas, w, h, ratio) {
-    if (!ratio) { ratio = PIXEL_RATIO; }
-  
+
+  function setCanvasSizeWithDisplayPixelRatio(canvas, w, h, ratio) {
+    if (!ratio) { ratio = MAX_DEFINITION_PIXEL_RATIO; }
+
     canvas.width = w * ratio;
     canvas.height = h * ratio;
     canvas.style.width = w + "px";
@@ -22,42 +22,42 @@ var PIXEL_RATIO = (function () {
     canvas.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
     return canvas;
   }
-  
+
   function renderTextLine(textLine) {
     const textHeight = 40;
-  
+
     const canvas = document.getElementsByClassName("trainingTextCanvas")[0];
     const ctx = canvas.getContext("2d");
-  
-    setHiDPICanvas(canvas, 1000, textHeight, 0.5);
-  
+
+    setCanvasSizeWithDisplayPixelRatio(canvas, 1000, textHeight, 0.5);
+
     ctx.textBaseline = "bottom";
     ctx.font = "bold " + textHeight + "px Courier New";
-  
+
     ctx.fillText(textLine, 0, textHeight);
   }
-  
+
   function renderRandomTrainingTextLine() {
     const rawTrainingTextElement = document.getElementsByClassName("rawTrainingText")[0];
     const trainingTextLines = rawTrainingTextElement.innerHTML.split(/\r?\n/);
-  
+
     const trainingTextLine = trainingTextLines[Math.floor(Math.random() * trainingTextLines.length)].trim()
     renderTextLine(trainingTextLine);
   }
-  
-  function getImageColumn(offset, ) {
+
+  function getImageColumn(offset) {
     // canvas.height is true image pixel height.
     // canvas.style.height is height on actual layout.  woot.
-  
+
     const canvas = document.getElementsByClassName("trainingTextCanvas")[0];
     const ctx = canvas.getContext("2d");
-  
+
     return ctx.getImageData(offset, 0, 1, canvas.height);
   }
-  
+
   function insertImageColumn(columnImageData, offset) {
     const canvas = document.getElementsByClassName("trainingTextCanvas")[0];
     const ctx = canvas.getContext("2d");
-    
+
     return ctx.putImageData(columnImageData, offset, 0);
   }
